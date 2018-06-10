d8d9a9d8153bc514a0329acc7348474b70126ec7
oncomouse <oncomouse@gmail.com>
2018-06-10 16:48:05 -0700

    Documentation

--- app/utilities/themeValueOrProp.js
+++ app/utilities/themeValueOrProp.js
@@ -1,4 +1,7 @@
 import { path } from 'ramda';
+/*
+  Returns the theme value stored at `themeKey` named in the prop `prop` OR return the value of the prop `prop`
+ */
 const themeValueOrProp = (themeKey, prop) => props =>
   path(
     ['theme', ...themeKey.split('.'), props[prop]],
