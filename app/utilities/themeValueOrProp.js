1bc97255d78457de659ddef0babc380fc1ffc281
oncomouse <oncomouse@gmail.com>
2018-06-22 11:05:56 -0500

    Lint

--- app/utilities/themeValueOrProp.js
+++ app/utilities/themeValueOrProp.js
@@ -1,10 +1,11 @@
 import { path } from 'ramda';
 /*
-  Returns the theme value stored at `themeKey` named in the prop `prop` OR return the value of the prop `prop`
+  Returns the theme value stored at `themeKey` named in the prop `prop` OR return the value of the
+  prop `prop`
  */
 const themeValueOrProp = (themeKey, prop) => props =>
   path(
     ['theme', ...themeKey.split('.'), props[prop]],
-    props
+    props,
   ) || props[prop];
 export default themeValueOrProp;
