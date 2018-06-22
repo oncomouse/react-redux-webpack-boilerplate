d47913d6cb1dbd32f8080db4d6688b5c0c999a83
oncomouse <oncomouse@gmail.com>
2018-06-22 09:12:11 -0500

    Comments

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 const webpackConfig = require('./webpack.config.js');
 
-webpackConfig.entry = () => ({});
+delete webpackConfig.entry;
 
 module.exports = (config) => {
   config.set({
@@ -13,16 +13,16 @@ module.exports = (config) => {
     preprocessors: {
       'tests.webpack.js': ['webpack', 'sourcemap'], // preprocess with webpack and our sourcemap loader
     },
-    reporters: ['mocha', 'coverage'], // report results in this format
-    webpack: webpackConfig,
+    reporters: ['mocha', 'coverage'], // report results in this format; change mocha to dots for shorter output
+    webpack: webpackConfig, // attach our webpack configuration
     webpackServer: {
       noInfo: true, // please don't spam the console when running in karma!
     },
     coverageReporter: {
       dir: 'coverage/',
       reporters: [
-        { type: 'lcov' },
-        { type: 'text' },
+        { type: 'lcov', subdir: '.' }, // Lcov gives good HTML results; Saved in ./coverage
+        { type: 'text' }, // Print a report to the console; can be text-summary
       ],
     },
   });
